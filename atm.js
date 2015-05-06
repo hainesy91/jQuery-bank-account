4afda715e91e3d7084f9f83b9c47cfc0b80b089d
Christopher Haines <hainesy91@gmail.com>
2015-05-06 17:51:53 +0100

    added input 1 console log

--- atm.js
+++ atm.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
-  
-})
 
+// var input = $('#amount1').val()
+//   console.log(input)
+
+
+$('#amount1').keyup(function(){
+  var input = ($(this).val());
+  console.log(input);
+});
+
+})
