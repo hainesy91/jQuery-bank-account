97fd820fe683f3471eb4d4c7fd7f919ee2936459
Christopher Haines <hainesy91@gmail.com>
2015-05-06 19:40:03 +0100

    added functionality to deposit and withdraw functions on both bank accounts

--- atm.js
+++ atm.js
@@ -4,9 +4,61 @@ $(document).ready(function(){
 //   console.log(input)
 
 
-$('#amount1').keyup(function(){
-  var input = ($(this).val());
-  console.log(input);
-});
+// $('#amount1').keyup(function(){
+  // var input = ($(this).val());
+
+//   console.log(input);
+// });
+
+// $('input').change(function(){
+
+// })
+
+var total = 0
+
+
+$('#deposit1').click(function(){
+  var number1 = $('#amount1').val();
+  total = total + parseInt(number1)
+  $('#balance1').html(total)
+})
+
+$('#withdraw1').click(function(){
+  var number1 = $('#amount1').val();
+  total1 = total - parseInt(number1)
+  $('#balance1').html(total1)
+})
+
+var total2 = 0
+
+$('#deposit2').click(function(){
+  var number2 = $('#amount2').val();
+  total2 = total2 + parseInt(number2)
+  $('#balance2').html(total2)
+})
+
+$('withdraw2').click(function(){
+  var number2 = $('#amount2').val();
+  total2 = total2 - parseInt(number2)
+  $('#balance2').html(total2)
+})
+
+
+
+  // total +('#amount1').html
+
+
+
+
+
+// currentbalance = function(){
+//   var deposit = $('#balance') + $('#deposit')
+//   console.log(currentbalance);
+
+
+// }
+
+
+
 
 })
