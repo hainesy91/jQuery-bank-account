9f0818afdb77c2f223f89982cd4668a8f58622f0
Christopher Haines <hainesy91@gmail.com>
2015-05-06 20:16:34 +0100

    got both bank statements to add and withdraw amounts!!!

--- atm.js
+++ atm.js
@@ -1,20 +1,9 @@
 $(document).ready(function(){
 
-// var input = $('#amount1').val()
-//   console.log(input)
-
-
-// $('#amount1').keyup(function(){
-  // var input = ($(this).val());
-
-//   console.log(input);
-// });
-
-// $('input').change(function(){
-
-// })
 
 var total = 0
+var total2 = 0
+var remainder = $('#balance1') + $('#deposit1').val()
 
 
 $('#deposit1').click(function(){
@@ -25,11 +14,10 @@ $('#deposit1').click(function(){
 
 $('#withdraw1').click(function(){
   var number1 = $('#amount1').val();
-  total1 = total - parseInt(number1)
-  $('#balance1').html(total1)
+  total = total - parseInt(number1)
+  $('#balance1').html(total)
 })
 
-var total2 = 0
 
 $('#deposit2').click(function(){
   var number2 = $('#amount2').val();
@@ -37,28 +25,11 @@ $('#deposit2').click(function(){
   $('#balance2').html(total2)
 })
 
-$('withdraw2').click(function(){
+$('#withdraw2').click(function(){
   var number2 = $('#amount2').val();
   total2 = total2 - parseInt(number2)
   $('#balance2').html(total2)
 })
-
-
-
-  // total +('#amount1').html
-
-
-
-
-
-// currentbalance = function(){
-//   var deposit = $('#balance') + $('#deposit')
-//   console.log(currentbalance);
-
-
-// }
-
-
 
 
 })
